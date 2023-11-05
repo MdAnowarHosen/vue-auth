9de6ec2bca863d528e71f2de074e254237de7fd0
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2023-11-05 14:35:51 +0600

    protect routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,81 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginPage from '../views/LoginPage.vue'
-import RegisterPage from '../views/RegisterPage.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LoginPage from "../views/LoginPage.vue";
+import RegisterPage from "../views/RegisterPage.vue";
+import store from "../store";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
+    meta: { requiresAuth: true },
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
   {
-    path: '/login',
-    name: 'login',
-    component: LoginPage
+    path: "/login",
+    name: "login",
+    component: LoginPage,
+    meta: { visitor: true },
   },
   {
-    path: '/register',
-    name: 'register',
-    component: RegisterPage
+    path: "/register",
+    name: "register",
+    component: RegisterPage,
+    meta: { visitor: true },
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+// router.beforeEach((to) => {
+//   // instead of having to check every route record with
+//   // to.matched.some(record => record.meta.requiresAuth)
+//   if (!store.getters.loggedIn && to.name !== 'login') {
+
+//     return {
+//      path: '/login',
+//      // name: '/login',
+//       // save the location we were at to come back later
+//       query: { redirect: to.fullPath },
+//     }
+//   }
+// })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    if (!store.getters.loggedIn) {
+      next({
+        name: "login",
+        query: { redirect: to.fullPath },
+      });
+    } else {
+      next();
+    }
+  } else if (to.matched.some((record) => record.meta.visitor)) {
+    if (store.getters.loggedIn) {
+      next({
+        name: "home",
+        query: { redirect: to.fullPath },
+      });
+    } else {
+      next();
+    }
+  } else {
+    next(); // make sure to always call next()!
+  }
+});
+export default router;
