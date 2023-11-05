e6ad09510188fcca1bb5b2b6979b53c3fda1a537
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2023-11-05 11:21:27 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+ <div>
+ <NavBar/>
+ <RouterView/>
+ <AppFooter/>
+ </div>
 </template>
+
+<script>
+
+import NavBar from './components/NavBar.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+
+  components: {
+    NavBar,
+    AppFooter
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -15,16 +29,5 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <RouterLink class="navbar-brand" :to="{name:'home'}">Auth App</RouterLink>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <RouterLink class="nav-link active" aria-current="page" :to="{name:'home'}">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" :to="{name:'about'}">About</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" :to="{name:'about'}">Login</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" :to="{name:'about'}">Register</RouterLink>
+        </li>
+      
+        <!-- <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li> -->
+      </ul>
+    </div>
+  </div>
+</nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'NavBar',
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
